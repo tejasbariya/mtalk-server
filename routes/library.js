24b2223633009b6bd334adb5af62247cd681bedc
tejasbariya <tejasbariya22@gmail.com>
2026-04-17 21:23:46 +0530

    fix: import of auth middleware in library.js

--- routes/library.js
+++ routes/library.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import LibraryEntry from '../models/LibraryEntry.js';
 import Title from '../models/Title.js';
-import { auth } from '../middleware/auth.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
