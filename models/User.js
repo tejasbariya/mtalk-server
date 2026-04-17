077fe29ac2b5b9345336601826583bb526a7075c
tejasbariya <tejasbariya22@gmail.com>
2026-04-17 21:54:50 +0530

    Add: Banner Url In User Model

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   avatar: { type: String, default: '' },
+  banner: { type: String, default: '' },
   bio: { type: String, default: '' },
   karma: { type: Number, default: 0 },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
