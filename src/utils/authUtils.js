c58bfbb077dc343f941e3ef0d1e18020e386757d
tejasbariya <tejasbariya22@gmail.com>
2026-05-01 15:36:35 +0530

    fix: syntax fix in authUtils (generateToken)

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -8,7 +8,7 @@ const generateToken = (userId) => {
     return jwt.sign(
         { id: userId },
         process.env.JWT_SECRET,
-        { expiresIn: '7`d' } // Users stay logged in for 30 days
+        { expiresIn: '7d' } // Users stay logged in for 30 days
     );
 };
 
